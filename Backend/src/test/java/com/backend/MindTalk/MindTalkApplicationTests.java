db2cedd55d072ff9cc7eba9abf532ba44cf7d778
RimazMoulana <ahmedrimaz99@gmail.com>
2023-06-26 00:34:48 +0530

    commit by rimaz 06/26 at 00:34

---
+++ Backend/src/test/java/com/backend/MindTalk/MindTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.MindTalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
