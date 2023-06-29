ea7b71b2aa4d1a56cb83bca1afe533d7475741aa
RimazMoulana <ahmedrimaz99@gmail.com>
2023-06-29 12:14:01 +0530

    add vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Navbar from './layout/Navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+     <h2>Hello MindTalk</h2>
+    </>
+  )
+}
+
+export default App
