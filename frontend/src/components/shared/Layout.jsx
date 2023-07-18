c1e4f8da8ab75960d7601f94f5fb111b59c5ec7b
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-07-18 18:15:25 -0400

    change outlet height by Kanishka

--- frontend/src/components/shared/Layout.jsx
+++ frontend/src/components/shared/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout() {
       <Sidebar />
       <div className='flex-1 h-screen flex flex-col'>
         <Header />
-        <div className='p-4 min-h-0 overflow-auto'>
+        <div className='p-4 h-full overflow-auto'>
           {<Outlet/>}
         </div>
       </div>
