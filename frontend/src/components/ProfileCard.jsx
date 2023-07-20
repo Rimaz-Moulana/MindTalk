431bde73104d42fe301ec8f55453db1790cf749a
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-07-20 10:03:02 +0530

    Pathum 10.02am 20/07/2023

--- frontend/src/components/ProfileCard.jsx
+++ frontend/src/components/ProfileCard.jsx
@@ -10,7 +10,7 @@ const ProfileCard = () => {
             <ProfilePhoto />
         </div>
         
-        <div className="mt-16 text-center">
+        <div className="mt-16 text-center ">
           <h3 className="ml-1 -mt-12 text-3xl font-bold leading-normal text-blueGray-700">
             Jenna Stones
           </h3>
@@ -18,7 +18,7 @@ const ProfileCard = () => {
             <i className="mr-2 text-lg fas fa-map-marker-alt text-blueGray-400"></i>
             Los Angeles, California
           </div>
-          <div className='flex items-center justify-center mt-2'>
+          <div className='flex items-center justify-center mt-2 max-'>
             <IconComponent />
           </div>
           <div className="mt-8 mb-2 italic fas fa-briefcase text- text-blueGray-600">
@@ -34,12 +34,12 @@ const ProfileCard = () => {
         <div className="px-5 py-10 mt-10 text-center border-t border-blueGray-200">
               <div className="flex justify-center ">
                 <div className="w-full ">
-                  <p className="mb-4 text-lg leading-relaxed text-justify text-blueGray-700">
+                  <p className="mb-4 text-lg leading-relaxed text-justify max-sm:text-base text-blueGray-700">
                     An artist of considerable range, Jenna the name taken
                     by Melbourne-raised, Brooklyn-based Nick Murphy
                     writes, performs and records all of his own music,
                     giving it a warm, intimate feel with a solid groove
-                    structure. An artist of considerable range.
+                    structure. 
                   </p>
                   <a href="javascript:void(0);" className="font-normal text-pink-500">
                     Show more
--- frontend/src/components/ProfileDetails.jsx
+++ frontend/src/components/ProfileDetails.jsx
@@ -3,34 +3,43 @@ import React from 'react'
 const ProfileDetails = () => {
   return (
     < >
-      <div className='px-5 '>
-        <div >
-          <h3 class="text-3xl font-bold mt-4 mb-4">General Information</h3>
-            <ul class="list-none list-inside">
-              <li><span class="font-bold">Name:</span> John Doe</li>
-              <li><span class="font-bold">Address:</span> 123 Main Street, City, Country</li>
-              <li><span class="font-bold">Mobile Phone:</span> 123-456-7890</li>
-              <li><span class="font-bold">Fixed Phone:</span> 987-654-3210</li>
-              <li><span class="font-bold">Email:</span> john.doe@example.com</li>
-              <li><span class="font-bold">District:</span> ABC District</li>
+      
+      <div className='w-full bg-top bg-no-repeat bg-cover h-60 rounded-t-xl bg-sky-500 max-md:hidden' 
+                                style={{background: 'url("https://source.unsplash.com/1500x250?mountain")'}}>
+                        </div>
+
+      <div className=''>
+        <div className='grid grid-cols-2 gap-5'>
+           <div className='z-50 p-5 mt-5 ml-5 transition-transform bg-blue-50 drop-shadow-lg rounded-xl'>
+                <h3 class="max-sm:2xl text-3xl font-bold mt-4 mb-4">General Information</h3>
+                <ul class="list-none list-inside mt-10">
+                    <li className='grid grid-cols-3'><span className="text-xl font-bold ">Name   :</span> <span className='col-span-2 ml-5 text-lg'> John Doe</span></li>
+                    <li className='grid grid-cols-3'><span className="text-xl font-bold ">Address   :</span> <span className='col-span-2 ml-5 text-lg'>123 Main Street, City, Country</span></li>
+                    <li className='grid grid-cols-3'><span className="text-xl font-bold ">Mobile Phone   :</span> <span className='col-span-2 ml-5 text-lg'>123-456-7890</span></li>
+                    <li className='grid grid-cols-3'><span className="text-xl font-bold ">Fixed Phone   :</span> <span className='col-span-2 ml-5 text-lg'>987-654-3210</span></li>
+                    <li className='grid grid-cols-3'><span className="text-xl font-bold ">Email   :</span> <span className='col-span-2 ml-5 text-lg'>john.doe@example.com</span></li>
+                    <li className='grid grid-cols-3'><span className="text-xl font-bold ">District   :</span> <span className='col-span-2 ml-5 text-lg'>ABC District</span></li>
+                </ul>
+            </div>
+
+        <div className='p-5 mt-5 mr-5 transition-transform bg-blue-50 drop-shadow-lg rounded-xl '>
+          <h3 class="max-sm:2xl text-3xl font-bold mt-4 mb-4">Professional Information</h3>
+            <ul class="list-none list-inside mt-10">
+              <li className='grid grid-cols-5'><span className="col-span-2 text-xl font-bold">Years of Experience   :</span> <span className='col-span-3 ml-5 text-lg'>5</span></li>
+              <li className='grid grid-cols-5'><span className="col-span-2 text-xl font-bold">Degree/Diploma   :</span> <span className='col-span-3 ml-5 text-lg'>Bachelor's Degree in Computer Science</span></li>
+              <li className='grid grid-cols-5'><span className="col-span-2 text-xl font-bold">Current Workplace   :</span> <span className='col-span-3 ml-5 text-lg'>ABC Company</span></li>
             </ul>
+            </div>
         </div>
 
-        <div>
-          <h3 class="text-3xl font-bold mt-4 mb-4">Professional Information</h3>
-            <ul class="list-none list-inside">
-              <li><span class="font-bold">Years of Experience:</span> 5</li>
-              <li><span class="font-bold">Degree/Diploma:</span> Bachelor's Degree in Computer Science</li>
-              <li><span class="font-bold">Current Workplace:</span> ABC Company</li>
-            </ul>
-        </div>
+       
 
-        <div>
-           <h3 class="text-2xl font-bold mt-4 mb-4">Contact</h3>
+        <div className='p-5 mt-5 ml-5 mr-5 transition-transform bg-blue-50 drop-shadow-lg rounded-xl'>
+           <h3 class="max-sm:2xl text-3xl font-bold mt-4 mb-4">Contact</h3>
             <ul class="list-none list-inside">
-              <li><span class="font-bold">Email:</span> john.doe@example.com</li>
-              <li><span class="font-bold">Phone:</span> 123-456-7890</li>
-              <li><span class="font-bold">Website:</span> www.example.com</li>
+              <li><span className="text-xl font-bold">Email   :</span> <span className='ml-5 text-lg'>john.doe@example.com</span></li>
+              <li><span className="text-xl font-bold">Phone   :</span> <span className='ml-5 text-lg'>123-456-7890</span></li>
+              <li><span className="text-xl font-bold">Website   :</span> <span className='ml-5 text-lg'>www.example.com</span></li>
             </ul>
         </div>
       </div>   
