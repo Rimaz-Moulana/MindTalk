4e4cc21a74c319f860534e3e23677ee41bf0f74c
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-07-24 13:42:07 -0400

    fix issues by kanishka

--- frontend/src/lib/consts/WalletNavigation.jsx
+++ frontend/src/lib/consts/WalletNavigation.jsx
@@ -1,28 +1,28 @@
-import {FiAirplay, FiMessageCircle,  FiPhone, FiUsers} from "react-icons/fi"
+import {FiAirplay, FiCheck, FiCheckCircle, FiCreditCard, FiDollarSign, FiPocket, FiUsers} from "react-icons/fi"
 
 export const WALLET_SIDEBAR_LINKS = [
 	{
 		key: 'wallet',
 		label: 'Wallet',
 		path: '/wallet',
-		icon: <FiAirplay />
+		icon: <FiPocket />
 	},
 	{
 		key: 'transhistory',
 		label: 'Transaction History',
 		path: '/transhistory',
-		icon: <FiMessageCircle />
+		icon: <FiDollarSign />
 	},
 	{
 		key: 'withdraw',
 		label: 'Withdraw',
 		path: '/withdraw',
-		icon: <FiPhone />
+		icon: <FiCreditCard />
 	},
 	{
 		key: 'dashoboard',
 		label: 'Dashboard',
 		path: '/',
-		icon: <FiUsers />
+		icon: <FiAirplay />
 	}
 ]
