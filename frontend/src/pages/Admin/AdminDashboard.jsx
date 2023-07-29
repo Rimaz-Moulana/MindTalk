069acb50f4516a6f1d3e0ea36debc9a8d470e48f
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-07-29 13:32:57 -0400

    Admin dashboard and User handling by kanishka

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import AdminCards from '../../components/AdminDashboard/AdminCards';
+import AdminTestGraph from '../../components/AdminDashboard/AdminTestGraph';
+import BarChart from '../../components/AdminDashboard/BarChart';
 
 const AdminDashboard = () => {
   return (
     <>
       <div className="flex flex-col gap-4 w-full">
-    
-     
         <div className="flex flex-wrap gap-4">
-         
+        <AdminCards />
         </div>
       
 
@@ -15,13 +16,13 @@ const AdminDashboard = () => {
         <div className="flex flex-wrap w-full">
           <div className="flex-1 max-w-md">
             <div className="bg-white rounded-xl shadow-md overflow-hidden text-center h-full mr-4">
-          
+            <AdminTestGraph />
             </div>
           </div>
 
           <div className="flex-1 max-w-full">
-            <div className="bg-white rounded-xl shadow-md overflow-hidden text-center p-5">
-           
+            <div className="bg-white rounded-xl shadow-md overflow-hidden text-center p-5 h-full">
+           <BarChart />
             </div>
           </div>
         </div>
