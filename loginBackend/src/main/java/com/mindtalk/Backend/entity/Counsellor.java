06de1ad15348b31921ad65d3076b99c19e83db5f
RimazMoulana <ahmedrimaz99@gmail.com>
2023-08-10 02:16:06 +0530

    commit by rimaz at 2;15

---
+++ loginBackend/src/main/java/com/mindtalk/Backend/entity/Counsellor.java
@@ -0,0 +1,82 @@
+package com.mindtalk.Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "counselors")
+public class Counselor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Long licenseNo;
+    private String licenseImage;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getLicenseNo() {
+        return licenseNo;
+    }
+
+    public void setLicenseNo(Long licenseNo) {
+        this.licenseNo = licenseNo;
+    }
+
+    public String getLicenseImage() {
+        return licenseImage;
+    }
+
+    public void setLicenseImage(String licenseImage) {
+        this.licenseImage = licenseImage;
+    }
+}
