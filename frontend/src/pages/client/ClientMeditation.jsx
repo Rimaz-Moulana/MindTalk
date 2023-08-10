fa6650d31b2f0f6285f1a19ddbf551893e2eaf4b
michellenikeetha <michellenikeetha@gmail.com>
2023-08-10 18:29:06 +0530

    Michelle 6.28pm 10/08/2023

--- frontend/src/pages/client/ClientMeditation.jsx
+++ frontend/src/pages/client/ClientMeditation.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-function ClientMeditation() {
+const videoLinks = [
+  'https://www.youtube.com/embed/hAkgumAMOIg',
+  'https://www.youtube.com/embed/another-video-link',
+  // Add more video links here
+];
+
+function ClientMusic() {
   return (
     <div className='flex-flex-row'>
 
       <div className="flex flex-col gap-4 w-full bg-white rounded-xl mb-5">
         <h1 className='text-xl font-bold pt-5 pl-5'>Meditation / Breathing Exercises</h1>
         <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 gap-y-8 p-5 pt-0'>
-            <iframe className='w-full aspect-[3/2]' src='https://www.youtube.com/embed/hAkgumAMOIg' title='video1'> </iframe>
-            <iframe className='w-full aspect-[3/2]' src='https://www.youtube.com/embed/hAkgumAMOIg' title='video1'> </iframe>
-            <iframe className='w-full aspect-[3/2]' src='https://www.youtube.com/embed/hAkgumAMOIg' title='video1'> </iframe>
-            <iframe className='w-full aspect-[3/2]' src='https://www.youtube.com/embed/hAkgumAMOIg' title='video1'> </iframe>
-            <iframe className='w-full aspect-[3/2]' src='https://www.youtube.com/embed/hAkgumAMOIg' title='video1'> </iframe>
-            <iframe className='w-full aspect-[3/2]' src='https://www.youtube.com/embed/hAkgumAMOIg' title='video1'> </iframe>
+          {videoLinks.map((link, index) => (
+            <iframe key={index} className='w-full aspect-[3/2]' src={link} title={`video${index + 1}`} allowFullScreen> </iframe>
+          ))}
         </div>
       </div>
   
     </div>
-  )
+  );
 }
 
-export default ClientMeditation
+export default ClientMusic;
+
