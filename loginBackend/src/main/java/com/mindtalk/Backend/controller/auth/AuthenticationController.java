b61519eda18beb52e8f92acdcaa938b957d0ec2b
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-08-12 23:28:23 -0400

    Implementing logout by Kanishka

--- loginBackend/src/main/java/com/mindtalk/Backend/controller/auth/AuthenticationController.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/controller/auth/AuthenticationController.java
@@ -1,9 +1,11 @@
 package com.mindtalk.Backend.controller.auth;
 
 import com.mindtalk.Backend.service.AuthenticationService;
+import com.mindtalk.Backend.service.LogoutService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,10 @@ import java.io.IOException;
 public class AuthenticationController {
 
     private final AuthenticationService service;
+
+    private final LogoutService logoutService;
+
+
 
     @PostMapping("/register")
     @CrossOrigin(origins = "http://127.0.0.1:5173", allowCredentials = "true") // Add this line
@@ -39,5 +45,16 @@ public class AuthenticationController {
             HttpServletResponse response
     ) throws IOException {
         service.refreshToken(request, response);
+    }
+
+    @PostMapping("/logout")
+    @CrossOrigin(origins = "http://127.0.0.1:5173", allowCredentials = "true")
+    public ResponseEntity<String> logout(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication
+    ) {
+        logoutService.logout(request, response, authentication);
+        return ResponseEntity.ok("Logged out successfully");
     }
 }
