2885d191e49ed03f2997f3794c4056e48e9b4d97
michellenikeetha <michellenikeetha@gmail.com>
2023-08-16 16:48:26 +0530

    michelle 4.48 16/08/2023

--- frontend/src/components/ClientCards.jsx
+++ frontend/src/components/ClientCards.jsx
@@ -5,59 +5,37 @@ const ClientCards = ({ clientList }) => {
   clientList && console.log(clientList);
 
   return (
-    
-        // <Link
-        //   key={index}
-        //   to={"profile"}
-        //   // to={`/profile/${contact.name.first}-${contact.name.last}`}
-        //   className="block"
-        // >
-        // <figure key={index} className='bg-white rounded-lg shadow-md pt-7 pb-7'>
-        //   <img
-        //     alt="user"
-        //     className='w-32 h-32 rounded-full mx-auto'
-        //     src={client.picture.large}
-        //   />
-        //   <figcaption className='text-center mt-5 flex-wrap'>
-        //     <p className='text-gray-700 font-semibold text-xl mb-2'>
-        //       {client.name.first} {client.name.last}
-        //     </p>
-        //   </figcaption>
-        // </figure>
-        // </Link>
+    <div className='w-full mx-auto bg-white shadow-lg rounded-md border border-gray-200'>
+      <table className="border-collapse w-full">
+        <tbody className='divide-y divide-gray-200'>
+          {clientList?.map((client, index) => (
+            <tr key={clientList.id}>
+              <td className="p-4">
+                <img
+                  alt="user"
+                  className='w-20 h-20 rounded-full mx-auto'
+                  src={client.picture.large}
+                />
+              </td>
+              <td className="p-4 whitespace-nowrap">
+                <span className='text-gray-800 font-semibold'>
+                  {client.name.first} {client.name.last}
+                </span>
+              </td>
+              <td className="p-4 flex space-x-2">
+                <Link
+                  to={"profile"}
+                  className="bg-blue-700 text-white px-4 py-2 rounded-md border font-semibold hover:bg-white hover:border-blue-700 hover:text-black flex items-center justify-center"
+                >
+                  View Profile
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-        <div className='w-full mx-auto bg-white shadow-lg rounded-sm border border-gray-200'>
-          <table className="border-collapse border border-gray-300 text-sm w-full">
-              {/* <thead>
-                  <tr className="p-2 border border-1 text-sm font-thin text-gray-500">
-                      <th className="border border-gray-300 p-2"></th>
-                      <th className="border border-gray-300 p-2">Name</th>
-                      <th className="border border-gray-300 p-2"></th>
-                  </tr>
-              </thead> */}
-              <tbody className='divide-y divide-gray-100'>
-                {clientList?.map((client, index) =>
-                      <tr key={clientList.id} >
-                          <td className="p-2 "><img
-                            alt="user"
-                            className='w-20 h-20 rounded-full mx-auto'
-                            src={client.picture.large}
-                          /></td>
-                          <td className="p-2 whitespace-nowrap">{client.name.first} {client.name.last}</td>
-                          <td className="p-2 flex space-x-1">
-                            <Link
-                                to={"profile"}
-                                className="bg-blue-700 p-2 text-white text-md rounded-md font-thin border hover:bg-white hover:border-blue-700 hover:text-black flex items-center justify-center"
-                            >
-                                View
-                            </Link>
-                          </td>
-
-                      </tr>
-                  )}
-              </tbody>
-          </table>
-        </div>
-  )
-                          }
 export default ClientCards;
