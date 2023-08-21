5936e1be462d155d081e1250c8bda6b1d26f4ad8
RimazMoulana <ahmedrimaz99@gmail.com>
2023-08-21 15:30:04 +0530

    Delete Backend directory
    backend deleted

--- Backend/src/main/java/com/mindtalk/Backend/repo/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.mindtalk.Backend.repo;
-
-import com.mindtalk.Backend.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-}
