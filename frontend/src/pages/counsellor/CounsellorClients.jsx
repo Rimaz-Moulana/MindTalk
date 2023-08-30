cc6803a09b99f2d73cfc9f1e5e92843c6311974c
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-08-30 20:00:30 +0530

    Pathum

--- frontend/src/pages/counsellor/CounsellorClients.jsx
+++ frontend/src/pages/counsellor/CounsellorClients.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import ClientCards from '../../components/ClientCards';
 import { Link } from 'react-router-dom';
+import ReactModal from 'react-modal';
+import RegisterClient from './CounsellorRegisterClient';
 import { img1 } from '../../assets';
 import { img2 } from '../../assets';
 import { img3 } from '../../assets';
@@ -13,7 +15,7 @@ const Clients = () => {
         last: 'Perera'
       },
       picture: {
-        medium: img2
+        medium: img1
       }
     },
     {
@@ -22,7 +24,7 @@ const Clients = () => {
         last: 'Lakshan'
       },
       picture: {
-        medium: img3
+        medium: img2
       }
     },
     {
@@ -31,10 +33,20 @@ const Clients = () => {
         last: 'Muthukuda'
       },
       picture: {
-        medium: img1
+        medium: img3
       }
     },
   ];
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   const [data, setData] = useState(initialClientData);
   const [clientList, setClientList] = useState(null);
@@ -62,24 +74,35 @@ const Clients = () => {
   return (
     <div className='bg-gray-100'>
       <section>
-        <div className='flex flex-row w-full flex-auto'>
+        <div className='flex flex-row flex-auto w-full'>
           <div className='flex-grow'>
             <form className='flex'>
               <input
                 type='text'
                 placeholder='Search...'
                 name='search'
-                className='ml-5 mt-6 rounded-md p-2'
+                className='p-2 mt-6 ml-5 rounded-md'
                 onChange={event => setFilterQuery(event.target.value)}
               />
             </form>
           </div>
           <div className='ml-auto'>
-            <Link to={'registerclient'}>
-              <button className='bg-blue-700 mt-6 rounded-md p-2 border text-white mr-5 hover:bg-white hover:border-blue-700 hover:text-black'>
-                Add Client
-              </button>
-            </Link>
+          <button
+              onClick={openModal}
+              className="p-2 mt-6 mr-5 text-white bg-blue-700 border rounded-md hover:bg-white hover:border-blue-700 hover:text-black"
+            >
+              Add Client
+            </button>
+            <ReactModal
+              isOpen={isModalOpen}
+              onRequestClose={closeModal}
+              contentLabel="Add Client Modal"
+              className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full modal"
+              overlayClassName="overlay fixed top-0 left-0 w-full h-full bg-black bg-opacity-50"
+            >
+              <RegisterClient closeModal={closeModal} />
+            </ReactModal>
+
           </div>
         </div>
       </section>
