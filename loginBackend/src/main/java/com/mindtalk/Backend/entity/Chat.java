ff2d468577e3b48fc860e4fd10667f1aa480ebc9
Thanduni <76160782+Thanduni@users.noreply.github.com>
2023-09-13 15:38:32 +0530

    create chat entity and desired other files

---
+++ loginBackend/src/main/java/com/mindtalk/Backend/entity/Chat.java
@@ -0,0 +1,30 @@
+package com.mindtalk.Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "_chats")
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Counsellor counsellor;
+
+//    @OneToMany(mappedBy = "_chats", cascade = CascadeType.ALL)
+//    private List<ChatMessage> messages = new ArrayList<>();
+
+}
