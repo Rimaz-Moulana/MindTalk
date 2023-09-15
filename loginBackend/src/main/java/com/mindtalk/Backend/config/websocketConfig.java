d6b72da8d83206e0a68943b0a50df09d1abf57e9
RimazMoulana <ahmedrimaz99@gmail.com>
2023-09-15 11:24:16 +0530

    commit by Rimaz

--- loginBackend/src/main/java/com/mindtalk/Backend/config/websocketConfig.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/config/websocketConfig.java
@@ -8,15 +8,17 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class websocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void configureMessageBroker(final MessageBrokerRegistry registry){
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //registry.addEndpoint("ws").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
-    public void registerStompEndpoints(final StompEndpointRegistry registry){
-        registry.addEndpoint("/ws").withSockJS();
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // registry.setApplicationDestinationPrefixes("/app");
+        // registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
     }
 }
