8d3a15f1e42b42076eee5410881a14ecaaa52773
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-09-18 13:12:09 +0530

    Pathum Blogs Edited

--- loginBackend/src/main/java/com/mindtalk/Backend/controller/entertainment/BlogsController.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/controller/entertainment/BlogsController.java
@@ -1,4 +1,76 @@
 package com.mindtalk.Backend.controller.entertainment;
 
+import com.mindtalk.Backend.dto.entertainment.BlogsDTO;
+import com.mindtalk.Backend.entity.entertainment.BlogsEntity;
+import com.mindtalk.Backend.service.entertainment.BlogsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/blogs")
 public class BlogsController {
+
+    @Autowired
+    private BlogsService blogsService;
+
+    @PostMapping
+    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+    public ResponseEntity<BlogsEntity> createBlogs(@RequestBody BlogsDTO blogsDTO){
+        BlogsEntity createdBlogs = blogsService.createBlogs(blogsDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdBlogs);
+    }
+
+//    @GetMapping("/{blogsId}")
+//    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+//    public ResponseEntity<BlogsEntity> getBlogsById(@PathVariable Integer blogsId){
+//        BlogsEntity blogs = blogsService.getBlogsById(blogsId);
+//
+//        if(blogs != null){
+//            return ResponseEntity.ok(blogs);
+//        }else{
+//            return (ResponseEntity<BlogsEntity>) ResponseEntity.noContent();
+//        }
+//    }
+
+//    @GetMapping("/all")
+//    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+//    public ResponseEntity<BlogsEntity> getAllBlogs(){
+//        BlogsEntity allBlogs = blogsService.getAllBlogs();
+//
+//        if(!allBlogs.isEmpty()){
+//            return ResponseEntity.ok(allBlogs);
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+
+//    @PutMapping("/{blogsId}")
+//    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+//    public ResponseEntity<BlogsEntity> updateBlogs(
+//            @PathVariable Integer blogsId,
+//            @RequestBody BlogsDTO blogsDTO){
+//        BlogsEntity updatedBlogs = blogsService.updateBlogs(blogsId, blogsDTO);
+//
+//        if(updatedBlogs != null){
+//            return ResponseEntity.ok(updatedBlogs);
+//        }else{
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+
+//    @PutMapping("/remove/{blogsId}")
+//    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+//    public ResponseEntity<BlogsEntity> removeBlogs(
+//            @PathVariable Integer blogsId,
+//            @RequestBody BlogsDTO blogsDTO){
+//        BlogsEntity removedBlogs = blogsService.removeBlogs(blogsId, blogsDTO);
+//
+//        if(removedBlogs != null){
+//            return ResponseEntity.ok(removedBlogs);
+//        }else{
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
 }
--- loginBackend/src/main/java/com/mindtalk/Backend/service/entertainment/BlogsService.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/service/entertainment/BlogsService.java
@@ -1,4 +1,26 @@
 package com.mindtalk.Backend.service.entertainment;
 
+import com.mindtalk.Backend.dto.entertainment.BlogsDTO;
+import com.mindtalk.Backend.entity.entertainment.BlogsEntity;
+import com.mindtalk.Backend.repo.entertainment.BlogsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BlogsService {
+
+    @Autowired
+    private BlogsRepo blogsRepo;
+
+
+    public BlogsEntity createBlogs(BlogsDTO blogsDTO){
+        BlogsEntity blogsEntity = new BlogsEntity();
+        blogsEntity.setTitle(blogsDTO.getCategory());
+        blogsEntity.setCategory(blogsDTO.getCategory());
+        blogsEntity.setContent(blogsDTO.getContent());
+
+        return blogsRepo.save(blogsEntity);
+    }
+
+
 }
