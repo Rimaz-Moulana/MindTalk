31da637a259437a82d9582f1cba9e963d62706ca
RimazMoulana <ahmedrimaz99@gmail.com>
2023-09-21 01:39:46 +0530

    rimaz

--- frontend/src/components/ModeratorDashboard/ModeratorButton.jsx
+++ frontend/src/components/ModeratorDashboard/ModeratorButton.jsx
@@ -4,7 +4,10 @@ import { FiCheck } from 'react-icons/fi';
 export default function ModeratorButton() {
     const [setModal, setModalOpen] = useState(false);
 
-    const [detailsData, setDetailsData] = useState([]);
+
+    const data = JSON.parse(localStorage.getItem('detailsData'));
+
+    console.log(data)
     
     return (
         <>
@@ -18,6 +21,7 @@ export default function ModeratorButton() {
             </button>
 
             {setModal ? (
+                
                 <>
                 <div className="fixed inset-0 z-10 overflow-y-auto">
                         <div
@@ -26,6 +30,7 @@ export default function ModeratorButton() {
                         ></div>
                         <div className="flex items-center h-screen px-4 py-8">
                             <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
+                            
                                 <div className="mt-3 sm:flex">
                                     {/* <div className="flex items-center justify-center flex-none w-12 h-12 mx-auto bg-red-100 rounded-full">
                                         {/* <svg
@@ -39,9 +44,12 @@ export default function ModeratorButton() {
                                                 d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
                                                 clipRule="evenodd"
                                             />
-                                        </svg> 
+                                        </svg>
                                     </div> */}
+
+                                    
                                     <div className="mt-2 justify-center sm:ml-4 sm:text-left">
+                                    
                                         <h4 className="text-lg font-medium text-gray-800">
                                             Couselllor Details
                                         </h4>
@@ -51,23 +59,30 @@ export default function ModeratorButton() {
                                             eiusmod tempor incididunt ut labore
                                             et dolore magna aliqua.
                                         </p> */}
-                            
+                                        
                                         <div className=''>
-                                        <label>First Name: {detailsData.map((entry) => {entry.firstName}) }</label>
-                                        <div className=""> 
-                                        <label>Lasst Name: {detailsData.map((entry) => {entry.lastName}) }</label>
+                                        {data.map((item,index)=>(
+                                            <div key={index}>
+                                            <div>
+                                            <label key={index}>First Name: {item.firstname}</label>
+                                            </div>
+                                            <div>
+                                            <label key={index}>Last Name: {item.lastname}</label>
+                                            </div>
+                                            <div>
+                                            <label key={index}>License Number: {item.licenseNo}</label>
+                                            </div>
+                                            <div>
+                                            <label key={index}>First Name: <img src={item.licenseImage} /></label>
+                                            </div>
+                                            </div>
+                                       
+                                            
+                                        ))} 
+                                        
+                                       
                                         </div>
-                                        <div className=""> 
-                                        <label>Email: {detailsData.map((entry) => {entry.email}) }</label>
-                                        </div>
-                                        <div className=''>
-                                        <label>lisence Number: {detailsData.map((entry) => {entry.lisenceNo}) }</label>
-                                        </div>
-                                        <div className=''>
-                                        <label>lisence Image: {detailsData.map((entry) => {entry.lisenceImage}) }</label>
-                                        </div>
-
-                                        </div>
+                                        
                                         <div className="items-center w-[100px] gap-2 mt-3 sm:flex">
                                             <button
                                                 className="w-full mt-2 p-2.5 flex-1 text-white bg-indigo-600 rounded-md outline-none ring-offset-2 ring-red-600 focus:ring-2"
@@ -91,8 +106,9 @@ export default function ModeratorButton() {
                             </div>
                         </div>
                     </div>
+                    
                 </>
-            ):null}
+           ):null}
         </>
     )
 }
