7ccd69687f4ccd4f38752230b5d00d57710be226
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-09-21 14:21:38 +0530

    Pathum ClientNoteSave

--- loginBackend/src/main/java/com/mindtalk/Backend/controller/client/ClientNoteController.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/controller/client/ClientNoteController.java
@@ -1,4 +1,30 @@
 package com.mindtalk.Backend.controller.client;
 
+import com.mindtalk.Backend.dto.client.ClientNoteDTO;
+import com.mindtalk.Backend.entity.client.ClientNoteEntity;
+import com.mindtalk.Backend.service.client.ClientNoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/client")
 public class ClientNoteController {
+
+    @Autowired
+    private ClientNoteService clientNoteService;
+
+    @PostMapping(value = "/savenote")
+    public ResponseEntity<ClientNoteEntity> saveClientNote(@RequestBody ClientNoteDTO clientNoteDTO){
+        ClientNoteEntity savedClientNote = clientNoteService.saveClientNote(clientNoteDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedClientNote);
+    }
+
+
+
+
 }
