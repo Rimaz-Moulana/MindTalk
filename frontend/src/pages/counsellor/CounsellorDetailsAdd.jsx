6e1b8b934fad6feceffffc56108b32186183ea87
RimazMoulana <ahmedrimaz99@gmail.com>
2023-09-23 00:02:13 +0530

    commit by rimaz

--- frontend/src/pages/counsellor/CounsellorDetailsAdd.jsx
+++ frontend/src/pages/counsellor/CounsellorDetailsAdd.jsx
@@ -49,74 +49,74 @@ const CounsellorDetailsAdd = ({onSubmit}) => {
               <div className="mb-12 p-14 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12 flex flex-col">
               <h1 className='text-2xl font-bold mb-4'>Add Your Details</h1>
               <form onSubmit={handeleSubmit}>
-      <div className='mb-4'>
-      <label htmlFor='firstName' className='block text-sm font-medium mb-1'>First Name</label>
-      <input
-        type='text'
-        id='firstName'
-        name='firstname'
-        value={firstname}
-        onChange={(e) => setFirstname(e.target.value)}
-        className='lg:w-[400px] px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300'
-
-      />
-      </div>
-      <div className='mb-4'>
-      <label htmlFor='lastName' className='block text-sm font-medium mb-1'>Last Name</label>
-      <input
-        type='text'
-        id='lastName'
-        name='lastname'
-        value={lastname}
-        onChange={(e) => setLastname(e.target.value)}
-        className='lg:w-[400px] px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300'
-
-      />
-      </div>
-      <div className='mb-4'>
-      <label htmlFor='email' className='block text-sm font-medium mb-1'>Email</label>
-      <input
-        type='email'
-        id='email'
-        name='email'
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className='lg:w-[400px] px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300'
-      />
-      </div>
-      <div className='mb-4'>
-      <label htmlFor='licenceNo' className='block text-sm font-medium mb-1'>License Number</label>
-      <input
-        type='text'
-        id='licenseNo'
-        name='licenseNo'
-        value={licenseNo}
-        onChange={(e) => setLicenseNo(e.target.value)}
-        className='lg:w-[400px] px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300'
-      />
-      </div>
-      <div className="mb-4">
-          <label htmlFor="licenseImage" className="block text-sm font-medium mb-1">
-            License Image
-          </label>
+          <div className='mb-4'>
+          <label htmlFor='firstName' className='block text-sm font-medium mb-1'>First Name</label>
           <input
-            type="file"
-            id="licenseImage"
-            name="licenseImage"
-            accept="image/*"
-            value={licenseImage}
-            onChange={(e) => setLicenseImage(e.target.value)}
-            className="px-3 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+            type='text'
+            id='firstName'
+            name='firstname'
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+            className='lg:w-[400px] px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300'
+
           />
-        </div>
-        <div className='lg:w-96'>
-        <button
-          type="submit"
-          className="bg-blue-500 lg:w-[400px] text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-        >
-          Submit
-        </button>
-      </div>
+          </div>
+          <div className='mb-4'>
+          <label htmlFor='lastName' className='block text-sm font-medium mb-1'>Last Name</label>
+          <input
+            type='text'
+            id='lastName'
+            name='lastname'
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+            className='lg:w-[400px] px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300'
+
+          />
+          </div>
+          <div className='mb-4'>
+          <label htmlFor='email' className='block text-sm font-medium mb-1'>Email</label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='lg:w-[400px] px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300'
+          />
+          </div>
+          <div className='mb-4'>
+          <label htmlFor='licenceNo' className='block text-sm font-medium mb-1'>License Number</label>
+          <input
+            type='text'
+            id='licenseNo'
+            name='licenseNo'
+            value={licenseNo}
+            onChange={(e) => setLicenseNo(e.target.value)}
+            className='lg:w-[400px] px-3 py-2 border border-gray-400 rounded-md focus:outline-none focus:ring focus:border-blue-300'
+          />
+          </div>
+          <div className="mb-4">
+              <label htmlFor="licenseImage" className="block text-sm font-medium mb-1">
+                License Image
+              </label>
+              <input
+                type="file"
+                id="licenseImage"
+                name="licenseImage"
+                accept="image/*"
+                value={licenseImage}
+                onChange={(e) => setLicenseImage(e.target.value)}
+                className="px-3 py-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+              />
+            </div>
+            <div className='lg:w-96'>
+            <button
+              type="submit"
+              className="bg-blue-500 lg:w-[400px] text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+            >
+              Submit
+            </button>
+          </div>
     </form>
     {/* <table>
         <thead>
