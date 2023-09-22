db10fd982790c13158800756777f070f5be321ac
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-09-22 11:04:24 +0530

    Pathum AddClientNote

--- loginBackend/src/main/java/com/mindtalk/Backend/service/client/ClientNoteService.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/service/client/ClientNoteService.java
@@ -6,6 +6,8 @@ import com.mindtalk.Backend.repo.client.ClientNoteRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClientNoteService {
 
@@ -16,9 +18,17 @@ public class ClientNoteService {
         ClientNoteEntity clientNoteEntity = new ClientNoteEntity();
         clientNoteEntity.setDate(clientNoteDTO.getDate());
         clientNoteEntity.setDuration(clientNoteDTO.getDuration());
-        clientNoteEntity.setSummary(clientNoteDTO.getSummary());
+        clientNoteEntity.setNote(clientNoteDTO.getNote());
 
         return clientNoteRepo.save(clientNoteEntity);
 
+    }
+
+    public List<ClientNoteEntity> getAllClientNote(){
+        return clientNoteRepo.findAll();
+    }
+
+    public ClientNoteEntity getClientNoteById(Integer clientNoteId){
+        return clientNoteRepo.findById(clientNoteId).orElse(null);
     }
 }
