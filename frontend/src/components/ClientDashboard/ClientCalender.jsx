d88f9851c1da44868b0d588d1259f5d67a3d7723
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-09-23 12:32:57 -0400

    set fetch data to clientcalender by Kanishka

--- frontend/src/components/ClientDashboard/ClientCalender.jsx
+++ frontend/src/components/ClientDashboard/ClientCalender.jsx
@@ -1,32 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import axios from 'axios';
 
 const localizer = momentLocalizer(moment);
 
-const appointmentData = [
-  {
-    title: 'Appointment 1',
-    start: new Date(2023, 7, 28, 10, 0), // year, month (0-indexed), day, hour, minute
-    end: new Date(2023, 7, 28, 11, 0),
-  },
-  {
-    title: 'Appointment 2',
-    start: new Date(2023, 7, 29, 15, 0),
-    end: new Date(2023, 7, 29, 16, 0),
-  },
-  // Add more appointment objects as needed
-];
+function ClientCalendar() {
+  const [events, setEvents] = useState([]);
 
-const ClientCalender = () => {
+  useEffect(() => {
+    fetchAppointments();
+  }, []);
+
+  const fetchAppointments = async () => {
+    try {
+      const authData = localStorage.getItem('authData');
+      if (authData) {
+        const { accessToken, id } = JSON.parse(authData);
+        const config = {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+          withCredentials: true,
+        };
+        const response = await axios.get(
+          `http://localhost:8080/api/client/appointment/get-appointments/${id}`,
+          config
+        );
+
+        if (response.status === 200) {
+          const appointments = response.data.map((appointment) => {
+            const startTime = moment(appointment.date + 'T' + appointment.timeSlot).format('hh:mma');
+            const endTime = moment(appointment.date + 'T' + appointment.timeSlot)
+              .add(1, 'hour')
+              .format('hh:mma');
+            const title = `${startTime}-${endTime} Appointment `;
+            return {
+              id: appointment.id,
+              title: title,
+              start: new Date(appointment.date + 'T' + appointment.timeSlot),
+              end: new Date(appointment.date + 'T' + appointment.timeSlot),
+            };
+          });
+          setEvents(appointments);
+        }
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
   return (
-    <div className="">
-      <h4 className="text-xl font-semibold "> Calendar</h4>
-      <div className="mt-4">
+    <div className="h-screen">
+      <div className="mt-4 h-full">
         <Calendar
           localizer={localizer}
-          events={appointmentData}
+          events={events}
           startAccessor="start"
           endAccessor="end"
           style={{ height: 500 }} // Set the height of the calendar
@@ -34,6 +65,6 @@ const ClientCalender = () => {
       </div>
     </div>
   );
-};
+}
 
-export default ClientCalender;
+export default ClientCalendar;
