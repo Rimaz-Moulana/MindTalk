5d6d8457b25755765c5d0d1b4f72c897142aef5c
Kanishka Sewwandi <109033047+Kanishk9907@users.noreply.github.com>
2023-09-23 20:07:45 -0400

    Changing counsellor logout header by kanishka

--- frontend/src/components/shared/CounsellorLayout.jsx
+++ frontend/src/components/shared/CounsellorLayout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import CounsellorSidebar from './CounsellorSidebar';
-import CounsellorHeader from './CounsellorHeader';
+import Header from './Header';
 
 export default function Layout() {
   return (
     <div className='flex flex-row bg-slate-100 h-screen w-screen overflow-hidden'>
       <CounsellorSidebar />
       <div className='flex-1 h-screen flex flex-col'>
-        <CounsellorHeader />
+        <Header />
         <div className='p-4 min-h-0 overflow-auto'>
           {<Outlet/>}
         </div>
