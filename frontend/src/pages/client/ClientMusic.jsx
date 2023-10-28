eb0155b19ae4ec0779048ed2c4e73644b618aceb
michellenikeetha <michellenikeetha@gmail.com>
2023-10-29 03:04:43 +0530

    no matching videos found message for music

--- frontend/src/pages/client/ClientMusic.jsx
+++ frontend/src/pages/client/ClientMusic.jsx
@@ -56,47 +56,22 @@ const ClientMusic = () => {
     setSearchKeyword(keyword);
   };
 
+  // Filter the music based on selected category and search keyword
+  const filteredMusic = music
+    .filter((item) => {
+      return (
+        (selectedCategory === 'all' || item.category === selectedCategory) &&
+        item.status &&
+        (searchKeyword === '' || item.description.toLowerCase().includes(searchKeyword.toLowerCase()))
+      );
+    });
+
   return (
-    // <div className='rounded-xl'>
-    //   {/* Add filtering and other UI elements here */}
-    //   <section className='md:flex gap-5 sm:gap-y-5'>
-    //     {/* Add filtering form or select elements here */}
-    //   </section>
-
-    //   <section className='pt-5 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 gap-y-8 p-5 pt-0'>
-    //     {music.map((item, index) => (
-    //       <iframe 
-    //         key={index} 
-    //         className='w-full aspect-[3/2]' 
-    //         src={item.link} 
-    //         title={`video${index + 1}`} 
-    //         allowFullScreen
-    //       > 
-    //       </iframe>
-    //     ))}
-    //   </section>
-    // </div>
-
     <div className='flex flex-row'>
-
       <div className='flex flex-col gap-4 w-full bg-white rounded-xl mb-5'>
         <h1 className='text-2xl font-bold pt-5 pl-5'>Music / Videos</h1>
-
-        {/* <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5 gap-y-8 p-5 pt-0'>
-          {music.map((item, index) => (
-            <iframe
-              key={index}
-              className="w-full aspect-[3/2]"
-              src={item.link}
-              title={`video${index+1}`}
-              allowfullscreen>
-            </iframe>
-          ))}
-        </div> */}
-
         <div className="flex flex-wrap gap-4 pl-5 pr-5 pb-2">
-          <div >
-            {/* <label className="pr-2">Filter by Category:</label> */}
+          <div>
             <select
               value={selectedCategory}
               onChange={(e) => filterMusicByCategory(e.target.value)}
@@ -110,9 +85,7 @@ const ClientMusic = () => {
               <option value="Stress Releasing">Stress Releasing</option>
             </select>
           </div>
-
-          <div >
-            {/* <label className="pr-2">Search by Description:</label> */}
+          <div>
             <input
               type="text"
               value={searchKeyword}
@@ -123,42 +96,30 @@ const ClientMusic = () => {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-5 pb-5">
-        {music
-            .filter((item) => {
-              return (
-                (selectedCategory === 'all' || item.category === selectedCategory) &&
-                item.status &&
-                (searchKeyword === '' || item.description.toLowerCase().includes(searchKeyword.toLowerCase()))
-              );
-            })
-          .map((item, index) => (
-            <div key={item.id} className="bg-gray-100 p-4 rounded-lg shadow-lg">
-              <div className="aspect-w-16 aspect-h-9 mb-4"> {/* Updated aspect ratio here */}
-                <iframe
-                  title={item.title}
-                  src={item.link}
-                  allowFullScreen
-                  controls
-                  className="w-full h-full rounded-md shadow-md"
-                >
-                </iframe>
-                {/* <embed
-                  src={item.link}
-                  wmode="opaque"
-                  type='video/mp4'
-                  width='100%'
-                  height='100%'
-                  allow='autoplay; encrypted-media; picture-in-picture'
-                  title={item.title}
-                /> */}
+        {filteredMusic.length === 0 ? (
+          <div className="p-5 text-center">
+            <p className="text-gray-500 text-xl font-semibold">No matching videos found.</p>
+            <p className="text-gray-400 mt-2">Try refining your search criteria.</p>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-5 pb-5">
+            {filteredMusic.map((item, index) => (
+              <div key={item.id} className="bg-gray-100 p-4 rounded-lg shadow-lg">
+                <div className="aspect-w-16 aspect-h-9 mb-4">
+                  <iframe
+                    title={item.title}
+                    src={item.link}
+                    allowFullScreen
+                    controls
+                    className="w-full h-full rounded-md shadow-md"
+                  ></iframe>
+                </div>
+                <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
+                <p className="text-gray-600 mb-2">{item.category}</p>
               </div>
-              <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
-              <p className="text-gray-600 mb-2">{item.category}</p>
-              {/* <p className="text-gray-700 mb-4">{item.description}</p> */}
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
