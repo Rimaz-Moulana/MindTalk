6355b75933021d674c03edae227470887286e17a
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-10-30 11:21:41 +0530

    retrieving profile details counsellor by pathum

--- loginBackend/src/main/java/com/mindtalk/Backend/service/CounsellorInfoService.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/service/CounsellorInfoService.java
@@ -9,7 +9,13 @@ import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
@@ -93,4 +99,51 @@ public class CounsellorInfoService {
         return null; //counsellor not found
     }
 
+//    public String updateProfilePhoto(Integer user_id, MultipartFile profilePhoto) {
+//        // Find the client by user_id
+//        Counsellor existingCounsellor = counsellorRepository.findByUserId(user_id).orElse(null);
+//
+//        if (existingCounsellor != null) {
+//            // Delete the existing profile photo if it exists
+//            if (existingCounsellor.getProfilePhotoPath() != null) {
+//                Path existingPhotoPath = Paths.get(profilePhotoUploadPath, existingCounsellor.getProfilePhotoPath());
+//                try {
+//                    Files.deleteIfExists(existingPhotoPath);
+//                } catch (IOException e) {
+//                    e.printStackTrace(); // Handle the exception if necessary
+//                }
+//            }
+//
+//            // Handle the new profile photo
+//            if (profilePhoto != null && !profilePhoto.isEmpty()) {
+//                try {
+//                    // Save the new profile photo to a local file
+//                    String fileName = System.currentTimeMillis() + "_" + profilePhoto.getOriginalFilename();
+//                    Path filePath = Paths.get(profilePhotoUploadPath, fileName);
+//                    Files.copy(profilePhoto.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+//
+//                    // Set the new profile photo path in the database
+//                    existingCounsellor.setProfilePhotoPath(fileName);
+//
+//                    // Save the updated client to update the profile photo path
+//                    counsellorRepository.save(existingCounsellor);
+//
+//                    return fileName; // Return the new profile photo path
+//                } catch (IOException e) {
+//                    e.printStackTrace(); // Handle the exception
+//                }
+//            }
+//        }
+//    return null; // Client not found or profile photo not updated
+//}
+
+//    public boolean deleteCounsellor(Integer counsellorId){
+//        Counsellor existingCounsellor = CounselorRepository.findById(counsellorId).orElse(null);
+//
+//        if (existingCounsellor != null ){
+//            counsellorRepository.delete(existingCounsellor);
+//            return true; //client Deleted
+//        }
+//        return false; //client not deleted
+//    }
 }
