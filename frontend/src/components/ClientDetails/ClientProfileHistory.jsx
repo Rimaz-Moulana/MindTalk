4a493e6965737ec9925b78aebd18c1958afebab5
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-10-31 13:19:12 +0530

    Pathum counsellorclient

--- frontend/src/components/ClientDetails/ClientProfileHistory.jsx
+++ frontend/src/components/ClientDetails/ClientProfileHistory.jsx
@@ -226,6 +226,8 @@ const ClientProfileHistory = ({testDataList}) => {
             </div>
           </div>
         </div>
+        <hr />
+        <hr  />
       </div>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-6">
