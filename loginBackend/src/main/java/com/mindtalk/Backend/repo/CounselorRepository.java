c69af3fc438a2e65fc4f248eb332f2c5916ab15a
Thanduni <76160782+Thanduni@users.noreply.github.com>
2023-10-31 12:14:57 +0530

    get counsellor id from user id

--- loginBackend/src/main/java/com/mindtalk/Backend/repo/CounselorRepository.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/repo/CounselorRepository.java
@@ -2,6 +2,11 @@ package com.mindtalk.Backend.repo;
 
 import com.mindtalk.Backend.entity.Counsellor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CounselorRepository extends JpaRepository<Counsellor,Long> {
+    @Query("SELECT c.id FROM Counsellor c WHERE c.user.id = :userId")
+    Integer findCounsellorIdByUserId(@Param("userId") Integer userId);
+
 }
