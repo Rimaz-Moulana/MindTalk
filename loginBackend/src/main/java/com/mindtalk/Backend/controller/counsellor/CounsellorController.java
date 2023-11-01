6a6671a768da1e285297566ad89dfa179d2cea2f
michellenikeetha <michellenikeetha@gmail.com>
2023-11-01 13:09:55 +0530

    counsellor profile backend by michelle

--- loginBackend/src/main/java/com/mindtalk/Backend/controller/counsellor/CounsellorController.java
+++ loginBackend/src/main/java/com/mindtalk/Backend/controller/counsellor/CounsellorController.java
@@ -168,23 +168,21 @@ public class CounsellorController {
 //        }
 //    }
 
-    // @PutMapping("/updateProfilePhoto/{user_id}")
-    // @CrossOrigin(origins = "${app.cors.allowed-origins}", allowCredentials =
-    // "true")
-    // public ResponseEntity<Counsellor> updateProfilePhoto(
-    // @PathVariable Integer user_id,
-    // @RequestPart(required = false) MultipartFile profilePhoto) {
-    //
-    // if (profilePhoto != null) {
-    // String updatedProfilePhotoPath =
-    // counsellorInfoService.updateProfilePhoto(user_id, profilePhoto);
-    // if (updatedProfilePhotoPath != null) {
-    // return ResponseEntity.ok(counsellorInfoService.getClientByUserId(user_id));
-    // }
-    // }
-    //
-    // return ResponseEntity.badRequest().build(); //Handle errors as needed
-    // }
+     @PutMapping("/updateProfilePhoto/{user_id}")
+     @CrossOrigin(origins = "${app.cors.allowed-origins}", allowCredentials = "true")
+     public ResponseEntity<Counsellor> updateProfilePhoto(
+         @PathVariable Integer user_id,
+         @RequestPart(required = false) MultipartFile profilePhoto) {
+
+     if (profilePhoto != null) {
+         String updatedProfilePhotoPath = counsellorInfoService.updateProfilePhoto(user_id, profilePhoto);
+         if (updatedProfilePhotoPath != null) {
+            return ResponseEntity.ok(counsellorInfoService.getCounsellorByUserId(user_id));
+         }
+     }
+
+     return ResponseEntity.badRequest().build(); //Handle errors as needed
+     }
 
     // @DeleteMapping("/{counsellorId}")
     // @CrossOrigin(origins = "${app.cors.allowed-origins}", allowCredentials =
